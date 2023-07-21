3211a3b57284854fc4d1a766a8bf0dc310d18c52
SimplyNate <horiuchi.nathan@gmail.com>
2023-07-20 17:46:38 -0700

    Define player stats

---
+++ src/components/player-stats.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+interface Player {
+    Health: number;
+    'Regen Cap': number;
+    'Regen Speed': number;
+    'Melee Resistance': number;
+    'Projectile Resistance': number;
+    'Infection Resistance': number;
+    'Revive Speed': number;
+    'Med Efficiency': number;
+    'Supply Efficiency': number;
+    'Main Ammo': number;
+    'Special Ammo': number;
+    'Tool Ammo': number;
+    'Melee Damage': number;
+    'Main Damage': number;
+    'Special Damage': number;
+    'C-Foam Portion': number;
+    'Sentry CPU Speed': number;
+    'Sentry Damage': number;
+    'SR Sentry Damage': number;
+    'Trip Mine Damage': number;
+    'Glow Stick Power': number;
+    'Fog Repeller Power': number;
+    'Tracker CPU Speed': number;
+    'Hacking Speed': number;
+    'Bioscan Speed': number;
+}
+const player: Player =  {
+    Health: 100,
+    'Regen Cap': 20,
+    'Regen Speed': 1,
+    'Melee Resistance': 1,
+    'Projectile Resistance': 1,
+    'Infection Resistance': 1,
+    'Revive Speed': 1,
+    'Med Efficiency': 1,
+    'Supply Efficiency': 1,
+    'Main Ammo': 1,
+    'Special Ammo': 1,
+    'Tool Ammo': 1,
+    'Melee Damage': 1,
+    'Main Damage': 1,
+    'Special Damage': 1,
+    'C-Foam Portion': 1,
+    'Sentry CPU Speed': 1,
+    'Sentry Damage': 1,
+    'SR Sentry Damage': 1,
+    'Trip Mine Damage': 1,
+    'Glow Stick Power': 1,
+    'Fog Repeller Power': 1,
+    'Tracker CPU Speed': 1,
+    'Hacking Speed': 1,
+    'Bioscan Speed': 1,
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
