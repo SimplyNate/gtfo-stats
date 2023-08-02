3636321cf06253a3ba1bd39837f19f81620276a3
SimplyNate <horiuchi.nathan@gmail.com>
2023-08-01 20:46:10 -0700

    Begin refactoring tool

--- src/data/tool.ts
+++ src/data/tool.ts
@@ -16,6 +16,48 @@ export interface SentryTool {
     'Biotracked Ammo Usage Multiplier': number;
 }
 
+export class EnhancedSentryTool {
+    [index: string]: any;
+    public tool: SentryTool;
+    private sentryDamageModifier: number;
+    private sentryAmmoModifier: number;
+    private sentryCPUModifier: number;
+    constructor(tool: SentryTool) {
+        this.tool = tool;
+        this.sentryCPUModifier = 1;
+        this.sentryDamageModifier = 1;
+        this.sentryAmmoModifier = 1;
+    }
+    get name(): string {
+        return this.tool.Name;
+    }
+    get type(): string {
+        return this.tool.Type;
+    }
+    set damageModifier(value: number) {
+        this.sentryDamageModifier = 1 + value;
+    }
+    set ammoModifier(value: number) {
+        this.sentryAmmoModifier = 1 + value;
+    }
+    set speedModifier(value: number) {
+        this.sentryCPUModifier = 1 + value;
+    }
+    get damage() {
+        return this.tool.Damage * this.sentryDamageModifier;
+    }
+    get precision() {
+        return this.tool.Damage * this.tool["Precision Multiplier"] * this.sentryDamageModifier;
+    }
+    get stagger() {
+        return this.tool.Damage * this.tool["Stagger Multiplier"] * this.sentryDamageModifier;
+    }
+}
+
+export class Tool {
+    tool: EnhancedSentryTool;
+}
+
 import _tools from './tool.json';
 const tools: SentryTool[] = _tools;
 
