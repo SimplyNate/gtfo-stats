5cacef8f522517c1c9f3a93240286a5dfe30fc00
SimplyNate <horiuchi.nathan@gmail.com>
2023-08-08 20:33:15 -0700

    Add Line Chart scaffolds

---
+++ src/components/EnemyDamageChart.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LineChart from './LineChart.vue';
+import { onMounted, computed, watch } from 'vue';
+
+const props = defineProps<{
+
+}>();
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { onMounted, computed, watch } from 'vue';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import Chart from 'chart.js/auto';
+
+Chart.register(ChartDataLabels);
+Chart.defaults.color = '#d3f7ff';
+
+const props = defineProps<{
+    datasets: any[];
+    labels: any[];
+    title: string;
+}>();
+
+let chart: Chart;
+
+onMounted(() => {
+    const ctx = <HTMLCanvasElement>document.getElementById(props.title + 'lineChart');
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: props.labels,
+            datasets: props.datasets,
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+            },
+        },
+    });
+});
+
+</script>
+
+<template>
+    <canvas :id="title + 'lineChart'"></canvas>
+</template>
+
+<style scoped>
+
+</style>
