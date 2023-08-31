4ea619480a2fea249beac086f75e103c953bba1e
SimplyNate <horiuchi.nathan@gmail.com>
2023-08-30 18:10:08 -0700

    Hook up selection, change calculation based on selection

--- src/components/EnemyKillChart.vue
+++ src/components/EnemyKillChart.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Enemy } from '../data/enemies';
 import { mainWeapons, specialWeapons } from '../data/weapons';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import BarChart from './BarChart.vue';
 import { totalKill } from '../computations';
 
@@ -25,8 +25,15 @@ const barData = computed(() => {
     };
     const health = props.enemy.Health;
     const data = [];
-    for (const weapon of weapons.value) {
-        data.push({label: weapon.Type, value: totalKill(health, weapon.damage, weapon.equipment['Max Ammo'])});
+    if (weakPointSelection.value === 'Body') {
+        for (const weapon of weapons.value) {
+            data.push({label: weapon.Type, value: totalKill(health, weapon.damage, weapon.equipment['Max Ammo'])});
+        }
+    }
+    else {
+        for (const weapon of weapons.value) {
+            data.push({label: weapon.Type, value: totalKill(health, weapon.precision * props.enemy["Weak Points"][weakPointSelection.value].Multiplier, weapon.equipment['Max Ammo'])});
+        }
     }
     data.sort((a, b) => b.value - a.value);
     for (const datum of data) {
@@ -35,13 +42,19 @@ const barData = computed(() => {
     }
     return { datasets: [dataset], labels };
 });
+
+const weakPoints = computed(() => {
+    return Object.keys(props.enemy['Weak Points']);
+});
+
+const weakPointSelection = ref<string>('Body');
 </script>
 
 <template>
     <div>
-        <select class="form-select w-50">
+        <select class="form-select w-50" v-model="weakPointSelection">
             <option>Body</option>
-            <option v-for="weakPoint of Object.keys(enemy['Weak Points'])" :key="weakPoint">{{ weakPoint }}</option>
+            <option v-for="weakPoint of weakPoints" :key="weakPoint">{{ weakPoint }}</option>
         </select>
     </div>
     <bar-chart :datasets="barData.datasets" :labels="barData.labels" />
