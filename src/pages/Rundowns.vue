f917167de44a88897496b7ed5b756cb0421aa531
SimplyNate <horiuchi.nathan@gmail.com>
2023-11-27 12:00:59 -0800

    Update names of expeditions to reflect true names

--- src/pages/Rundowns.vue
+++ src/pages/Rundowns.vue
@@ -11,77 +11,77 @@ interface RundownData {
 
 const rundownData = ref<RundownData>({
     R1: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986457817123/91ED86D4D8522DE7CBBD1F9F4248E3D7BB677213/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986458158672/34E1989A010AB2FD196DD4EAC6D35D89ABFEF238/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986458539018/852AD279464FC4FD39E932A74951923DDA81C59A/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986462082834/A020ADB4C014FBDD8469C0AD74AA607238751C5E/',
-        'ALT://C2': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986462477140/490D176A198A8890AEDBFBC78C57A0293C60FCD9/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986463153320/1CB7656D1187CE34F193C63E8AFC0A573AE9DC70/',
+        'ALT://R1A1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986457817123/91ED86D4D8522DE7CBBD1F9F4248E3D7BB677213/',
+        'ALT://R1B1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986458158672/34E1989A010AB2FD196DD4EAC6D35D89ABFEF238/',
+        'ALT://R1B2': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986458539018/852AD279464FC4FD39E932A74951923DDA81C59A/',
+        'ALT://R1C1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986462082834/A020ADB4C014FBDD8469C0AD74AA607238751C5E/',
+        'ALT://R1C2': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986462477140/490D176A198A8890AEDBFBC78C57A0293C60FCD9/',
+        'ALT://R1D1': 'https://steamuserimages-a.akamaihd.net/ugc/1922499986463153320/1CB7656D1187CE34F193C63E8AFC0A573AE9DC70/',
     },
     R2: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919228656074/DDFA510078649FF8B8D662A972A9D97452CAEAA7/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919228953608/F589E56E2C5B09C4A54FE53FAC21A63D91E57500/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229255742/4AF33F1EF20E97EFE69620941D0D3A3B2D36C0BA/',
-        'ALT://B3': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229530141/B95495E79624A5E6E2DBAD40A0B894A735709F2F/',
-        'ALT://B4': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229757476/498A2DC3EC55F580D48122213033EBE46B121A79/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919230092951/B08B0D5BE4F12043DED7DCE04E572C7A06350722/',
-        'ALT://C2': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919234928479/E927A75981025ABE2B0078A7ED1D4B2E7A34707B/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449575097173/B60015DE529C362C5C12B1AC5365F31EDF44561B/',
-        'ALT://D2': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449579261421/67C590B51328A03A9628F0CDD2C3FBB52C65079F/',
-        'ALT://E1': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449579693855/63BE49F90F0E934F9A42817D0634DD4C2F78B64D/',
+        'ALT://R2A1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919228656074/DDFA510078649FF8B8D662A972A9D97452CAEAA7/',
+        'ALT://R2B1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919228953608/F589E56E2C5B09C4A54FE53FAC21A63D91E57500/',
+        'ALT://R2B2': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229255742/4AF33F1EF20E97EFE69620941D0D3A3B2D36C0BA/',
+        'ALT://R2B3': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229530141/B95495E79624A5E6E2DBAD40A0B894A735709F2F/',
+        'ALT://R2B4': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919229757476/498A2DC3EC55F580D48122213033EBE46B121A79/',
+        'ALT://R2C1': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919230092951/B08B0D5BE4F12043DED7DCE04E572C7A06350722/',
+        'ALT://R2C2': 'https://steamuserimages-a.akamaihd.net/ugc/2018205919234928479/E927A75981025ABE2B0078A7ED1D4B2E7A34707B/',
+        'ALT://R2D1': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449575097173/B60015DE529C362C5C12B1AC5365F31EDF44561B/',
+        'ALT://R2D2': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449579261421/67C590B51328A03A9628F0CDD2C3FBB52C65079F/',
+        'ALT://R2E1': 'https://steamuserimages-a.akamaihd.net/ugc/2018206449579693855/63BE49F90F0E934F9A42817D0634DD4C2F78B64D/',
     },
     R3: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145297098/2455AE8616B009877C464107F796040BF6B230C0/',
-        'ALT://A2': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145299606/040959F39F3277440002F3A90C77E295CF80DAC4/',
-        'ALT://A3': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145300572/A20423D92CDEFFC397B0EB56208C608D5B5DA7F8/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145301754/818F46282C85C729619EC2B3FD5656392F182B97/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145303229/4CE3722FE866D9F718482C2472A328ED977CEE5F/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145305864/E5A02FF9B7B60456A4E851084F15DCFCCAF1761C/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145306915/F8AB646ABBEB23D68C00481AEC1670EA6C9E1A88/',
+        'ALT://R3A1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145297098/2455AE8616B009877C464107F796040BF6B230C0/',
+        'ALT://R3A2': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145299606/040959F39F3277440002F3A90C77E295CF80DAC4/',
+        'ALT://R3A3': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145300572/A20423D92CDEFFC397B0EB56208C608D5B5DA7F8/',
+        'ALT://R3B1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145301754/818F46282C85C729619EC2B3FD5656392F182B97/',
+        'ALT://R3B2': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145303229/4CE3722FE866D9F718482C2472A328ED977CEE5F/',
+        'ALT://R3C1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145305864/E5A02FF9B7B60456A4E851084F15DCFCCAF1761C/',
+        'ALT://R3D1': 'https://steamuserimages-a.akamaihd.net/ugc/2049738728145306915/F8AB646ABBEB23D68C00481AEC1670EA6C9E1A88/',
     },
     R4: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162856869/9A7A7ABF0201CCFAFB1C5DF73DA208C6252B7916/',
-        'ALT://A2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162857636/D707CDA1E3A97E792A69F0B3E693DFA1229317D6/',
-        'ALT://A3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162859245/F056D6B373AADD45E90EC1E8EEE13DA74307F986/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162860579/7BE949DDE797CD4B5518898324F45F45E13A234A/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162861493/DABCCB85F4B54A5CFE32FAC180BBE763F2A3C4C1/',
-        'ALT://B3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162864020/14206F21B082570245F51C7E005BA13FB7997A7D/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162865087/D16BC1F5E8522BD27E9DFED60888393E46B63E80/',
-        'ALT://C2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162866023/A44A97552368BEB17099544C1CE1C67B618660E7/',
-        'ALT://C3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162867116/B33549AB8511B0B0952B2BA3AE19C1ADC830E712/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162868143/8173CD9DEE8963CD483D1D63458863BAE9AFE08C/',
-        'ALT://D2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162869100/AC3D6A2D4FF3FEF86FD8F366C1FC76155482D9E7/',
-        'ALT://E1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162870039/A34B2E4EB93F9E0A0009473D2A83540DA06A2183/',
+        'ALT://R4A1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162856869/9A7A7ABF0201CCFAFB1C5DF73DA208C6252B7916/',
+        'ALT://R4A2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162857636/D707CDA1E3A97E792A69F0B3E693DFA1229317D6/',
+        'ALT://R4A3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162859245/F056D6B373AADD45E90EC1E8EEE13DA74307F986/',
+        'ALT://R4B1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162860579/7BE949DDE797CD4B5518898324F45F45E13A234A/',
+        'ALT://R4B2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162861493/DABCCB85F4B54A5CFE32FAC180BBE763F2A3C4C1/',
+        'ALT://R4B3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162864020/14206F21B082570245F51C7E005BA13FB7997A7D/',
+        'ALT://R4C1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162865087/D16BC1F5E8522BD27E9DFED60888393E46B63E80/',
+        'ALT://R4C2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162866023/A44A97552368BEB17099544C1CE1C67B618660E7/',
+        'ALT://R4C3': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162867116/B33549AB8511B0B0952B2BA3AE19C1ADC830E712/',
+        'ALT://R4D1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162868143/8173CD9DEE8963CD483D1D63458863BAE9AFE08C/',
+        'ALT://R4D2': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162869100/AC3D6A2D4FF3FEF86FD8F366C1FC76155482D9E7/',
+        'ALT://R4E1': 'https://steamuserimages-a.akamaihd.net/ugc/2070009996162870039/A34B2E4EB93F9E0A0009473D2A83540DA06A2183/',
     },
     R5: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600131909/86698319C990BBC440F4CEFA46DC6A7C0AD8A9F6/',
-        'ALT://A2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600133267/304DDD1B067ED31F810B30A1835F4BFA7ACB4D65/',
-        'ALT://A3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600134506/C2D7CAE6C5ED85DC9658470907A6148DBC35BF5E/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600135309/B26F53B425CD8B33B20619187CB21DBA0B210BF6/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603483697/56E1AF72077E679B736939F4B10D8FA664B10451/',
-        'ALT://B3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603575457/CC51ADD9CEFEDA406C04450DD729EC4E6E37E101/',
-        'ALT://B4': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603699030/942FCCA716681920C066001D3A1E999A9E595C8B/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603802602/3CA75EE8209DE6365F0EF21C1BB2DDBE30692EC8/',
-        'ALT://C2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603951685/8EEAC8899F9F576988AD2CE75FE812FB5A6D69CB/',
-        'ALT://C3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604074452/46E1AC68C4559DB3D9F312743F97ADFEB65C280A/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604173537/476293F6A8278CBA2494AA836E92BE6A0E532484/',
-        'ALT://D2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604299138/22341518F7494AA824630CE6FB4E477E4064FF17/',
-        'ALT://E1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604438647/E195278BEC8D93E9A694B668292B949561ACD703/',
+        'ALT://R5A1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600131909/86698319C990BBC440F4CEFA46DC6A7C0AD8A9F6/',
+        'ALT://R5A2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600133267/304DDD1B067ED31F810B30A1835F4BFA7ACB4D65/',
+        'ALT://R5A3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600134506/C2D7CAE6C5ED85DC9658470907A6148DBC35BF5E/',
+        'ALT://R5B1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555600135309/B26F53B425CD8B33B20619187CB21DBA0B210BF6/',
+        'ALT://R5B2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603483697/56E1AF72077E679B736939F4B10D8FA664B10451/',
+        'ALT://R5B3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603575457/CC51ADD9CEFEDA406C04450DD729EC4E6E37E101/',
+        'ALT://R5B4': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603699030/942FCCA716681920C066001D3A1E999A9E595C8B/',
+        'ALT://R5C1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603802602/3CA75EE8209DE6365F0EF21C1BB2DDBE30692EC8/',
+        'ALT://R5C2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555603951685/8EEAC8899F9F576988AD2CE75FE812FB5A6D69CB/',
+        'ALT://R5C3': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604074452/46E1AC68C4559DB3D9F312743F97ADFEB65C280A/',
+        'ALT://R5D1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604173537/476293F6A8278CBA2494AA836E92BE6A0E532484/',
+        'ALT://R5D2': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604299138/22341518F7494AA824630CE6FB4E477E4064FF17/',
+        'ALT://R5E1': 'https://steamuserimages-a.akamaihd.net/ugc/2054252555604438647/E195278BEC8D93E9A694B668292B949561ACD703/',
     },
     R6: {
-        'ALT://A1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109190708/667A7F65C8898FADBD855F0C90A7BEB0A3ECEF8B/',
-        'ALT://AX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109198588/C7F0776FCF77629092041226DB7933D280EEA328/',
-        'ALT://B1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109193590/06E2EAC87A001804F64EAC69EBD029D37D8A9BAF/',
-        'ALT://B2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109196573/B9388298EF313936345E75A158ACA117124BBE0B/',
-        'ALT://BX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109205189/A1712B0E8B7B6FDAE898265D3E67EB47868C840F/',
-        'ALT://C1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112724559/1F1BEEB6D1C65B76888357BE7519D8C6F8368096/',
-        'ALT://C2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112857500/0FE82503DE8DBA53EB9F4E3756AE9F9FD4AF1E0D/',
-        'ALT://C3': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112999985/57DF816163060DAF061AA1124D783BCE52B42BE0/',
-        'ALT://CX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113089373/3801B4EA942638695EC3F295153388995FF3C842/',
-        'ALT://D1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113199594/96C536062078CA9DAEED7FB0804E618C018064CE/',
-        'ALT://D2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113283669/7F30A62CEEEE50B5009C5E82DAAA6AAB886C1CB0/',
-        'ALT://D3': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113433448/BF2DEB3DAEB8E9862DC5D40C3981841E479A549F/',
-        'ALT://D4': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113593099/064F677CF347A0E13739975F6F30B05BF4009701/',
+        'ALT://R6A1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109190708/667A7F65C8898FADBD855F0C90A7BEB0A3ECEF8B/',
+        'ALT://R6AX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109198588/C7F0776FCF77629092041226DB7933D280EEA328/',
+        'ALT://R6B1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109193590/06E2EAC87A001804F64EAC69EBD029D37D8A9BAF/',
+        'ALT://R6B2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109196573/B9388298EF313936345E75A158ACA117124BBE0B/',
+        'ALT://R6BX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909109205189/A1712B0E8B7B6FDAE898265D3E67EB47868C840F/',
+        'ALT://R6C1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112724559/1F1BEEB6D1C65B76888357BE7519D8C6F8368096/',
+        'ALT://R6C2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112857500/0FE82503DE8DBA53EB9F4E3756AE9F9FD4AF1E0D/',
+        'ALT://R6C3': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909112999985/57DF816163060DAF061AA1124D783BCE52B42BE0/',
+        'ALT://R6CX': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113089373/3801B4EA942638695EC3F295153388995FF3C842/',
+        'ALT://R6D1': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113199594/96C536062078CA9DAEED7FB0804E618C018064CE/',
+        'ALT://R6D2': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113283669/7F30A62CEEEE50B5009C5E82DAAA6AAB886C1CB0/',
+        'ALT://R6D3': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113433448/BF2DEB3DAEB8E9862DC5D40C3981841E479A549F/',
+        'ALT://R6D4': 'https://steamuserimages-a.akamaihd.net/ugc/2119562909113593099/064F677CF347A0E13739975F6F30B05BF4009701/',
     },
     R7: {
         A1: 'https://steamuserimages-a.akamaihd.net/ugc/1862817291122574903/1F063993285AB685970E4480DA390B330AF2D51D/',
@@ -111,7 +111,7 @@ function setSelectedRundown(rundown: string) {
         const previous = document.querySelector(`#rundowns-${selectedRundown.value}`);
         previous?.classList.remove('yellow');
         if (selectedExpedition.value) {
-            const expedition = document.querySelector(`#expeditions-${selectedRundown.value + selectedExpedition.value}`);
+            const expedition = document.querySelector(`#expeditions-${cleanExpeditionName(selectedExpedition.value)}`);
             expedition?.classList.remove('yellow');
         }
     }
@@ -125,11 +125,11 @@ const selectedImage = ref<string>();
 
 function setSelectedExpedition(expedition: string) {
     if (selectedExpedition.value) {
-        const previous = document.querySelector(`#expeditions-${selectedRundown.value + selectedExpedition.value}`);
+        const previous = document.querySelector(`#expeditions-${cleanExpeditionName(selectedExpedition.value)}`);
         previous?.classList.remove('yellow');
     }
     selectedExpedition.value = expedition;
-    const current = document.querySelector(`#expeditions-${selectedRundown.value + expedition}`);
+    const current = document.querySelector(`#expeditions-${cleanExpeditionName(expedition)}`);
     current?.classList.add('yellow');
     if (selectedRundown.value) {
         selectedImage.value = rundownData.value[selectedRundown.value][selectedExpedition.value]
@@ -137,6 +137,10 @@ function setSelectedExpedition(expedition: string) {
     else {
         selectedImage.value = undefined;
     }
+}
+
+function cleanExpeditionName(expedition: string) {
+    return expedition.replace('://', '---');
 }
 
 </script>
@@ -148,28 +152,28 @@ function setSelectedExpedition(expedition: string) {
             <div class="mb-2" style="font-size: 0.8em">Map credits: Hunter48RUS, Artefas, livinghell</div>
             <div class="d-flex">
                 <div class="me-3" style="width: 100px;">
-                    <h5 class="menu-item clickable"
+                    <h5 class="menu-item clickable p-2"
                         v-for="rundown of rundowns"
                         :key="rundown"
                         :id="`rundowns-${rundown}`"
                         @click="setSelectedRundown(rundown)"
                     >{{ rundown }}</h5>
                 </div>
-                <div v-if="selectedRundown" style="width: 100px;">
-                    <h5 class="menu-item clickable"
-                        v-for="key of expeditionList(selectedRundown)"
-                        :key="key"
-                        :id="`expeditions-${selectedRundown + key}`"
-                        @click="setSelectedExpedition(key)"
-                    >{{ selectedRundown + key }}</h5>
+                <div v-if="selectedRundown">
+                    <h5 class="menu-item clickable p-2"
+                        v-for="expedition of expeditionList(selectedRundown)"
+                        :key="expedition"
+                        :id="`expeditions-${cleanExpeditionName(expedition)}`"
+                        @click="setSelectedExpedition(expedition)"
+                    >{{ expedition }}</h5>
                 </div>
             </div>
         </div>
         <div class="ms-auto" v-if="selectedImage">
-            <img :src="selectedImage" :alt="`${selectedRundown}${selectedExpedition} Map`" style="max-width: 100%;"/>
+            <img :src="selectedImage" :alt="`${selectedExpedition} Map`" style="max-width: 100%;"/>
         </div>
     </div>
-</template>
+</template>R6
 
 <style scoped>
 .yellow {
